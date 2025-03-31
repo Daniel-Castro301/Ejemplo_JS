0add20c3bd458a7906d8264e18c50ee210c25ec8
daniel-castro301 <dacastro002@gmail.com>
2025-03-31 10:09:52 -0500

    31/03/2025(2)

---
+++ ejemplo.js
@@ -0,0 +1,48 @@
+console.log(eval (new String("2 + 2")));
+console.log("\n");
+console.log(eval("2 + 2"));
+
+var nombre1 ="Hola mundo";
+console.log(nombre1)
+
+var variable = "Nombre";
+let nombre;
+nombre = "Camilo";
+const apellido = "rocha";
+
+console.log(variable);
+console.log(nombre);
+console.log(apellido);
+
+
+let nombrecompleto2= "sebastian vanegas";
+let nombreCompleto2 = "camilo rocha";
+
+console.log(nombrecompleto2);
+console.log(nombreCompleto2);
+
+let _10 = "daniel puto";
+let $hola = 58;
+let a123 = false;
+
+let x,y;
+x=7
+y=44
+
+console.log(x,y,_10,$hola,a123);
+
+var simbolo = Symbol("mi simbolo")
+var z;
+console.log(z);
+
+let objeto ={
+    nombre: "daniel",
+    apellido:"castro",
+    tel:3212543838
+    }
+console.log(objeto) 
+
+console.log(objeto.nombre)
+console.log(objeto["nombre"])
+
+g
